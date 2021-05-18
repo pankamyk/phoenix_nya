c91537955065a1fa7076ab2115369a8ad7d5bf26
pankamyk <pankamyk11@gmail.com>
2021-05-19 00:59:35 +0200

    added auth error handling and guardian config

--- config/config.exs
+++ config/config.exs
@@ -7,25 +7,32 @@
 # General application configuration
 use Mix.Config
 
-config :nya,
-  ecto_repos: [Nya.Repo]
+config( :nya,
+  ecto_repos: [Nya.Repo] )
 
 # Configures the endpoint
-config :nya, NyaWeb.Endpoint,
+config( :nya, NyaWeb.Endpoint,
   url: [host: "localhost"],
   secret_key_base: "s8ciYg9FfbQ/9LIa5uPWAhxCx2mY1JqQIt225viLTIQNGdTKY/lHzHU9jmSb71Ef",
   render_errors: [view: NyaWeb.ErrorView, accepts: ~w(html json), layout: false],
   pubsub_server: Nya.PubSub,
-  live_view: [signing_salt: "nNDZ+BWX"]
+  live_view: [signing_salt: "nNDZ+BWX"] )
+
+# Configure Guardian
+config( :nya, NyaWeb.Guardian,
+  issuer: "nya",
+  secret_key: 
+    System.get_env("GUARDIAN_SECRET_KEY") 
+    || "dupa-Super_SECRET$$KeyeYErYEeE" )
 
 # Configures Elixir's Logger
-config :logger, :console,
+config( :logger, :console,
   format: "$time $metadata[$level] $message\n",
-  metadata: [:request_id]
+  metadata: [:request_id] )
 
 # Use Jason for JSON parsing in Phoenix
-config :phoenix, :json_library, Jason
+config( :phoenix, :json_library, Jason )
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
-import_config "#{Mix.env()}.exs"
+import_config("#{Mix.env()}.exs")
